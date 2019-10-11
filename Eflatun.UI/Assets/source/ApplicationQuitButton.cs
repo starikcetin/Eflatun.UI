2e4cf28385faa1ba7b93adf7bf98d42ec1750df2
starikcetin <cetinsamedtarik@gmail.com>
2019-10-11 21:24:16 +0300

    unity proj + code import + asmdef & package.json

---
+++ Eflatun.UI/Assets/source/ApplicationQuitButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Eflatun.UI.source
+{
+    /// <summary>
+    /// A Button that quits the application when clicked.
+    /// </summary>
+    public class ApplicationQuitButton : ButtonBase
+    {
+        protected override void OnClickListener()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Eflatun.UI/Assets/source/EventButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Eflatun.UI.source
+{
+    /// <summary>
+    /// Has an <see cref="OnClick"/> event that will get invoked when button is clicked.
+    /// </summary>
+    public class EventButton : ButtonBase
+    {
+        /// <summary>
+        /// This will get invoked when button is clicked.
+        /// </summary>
+        public event Action OnClick;
+
+        #region Overrides of ButtonBase
+
+        protected override void OnClickListener()
+        {
+            OnClick.InvokeSafe();
+        }
+
+        #endregion
+    }
+}
